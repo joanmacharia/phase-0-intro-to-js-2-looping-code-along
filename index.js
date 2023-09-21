cfae6cb1bed8e7585bfb81f00a49dbdd4bc95c89
Joan Macharia <joan.macharia@students.moringa>
2023-09-21 17:22:52 +0300

    Completed asssignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(name, eventName) {
+    let newArray = []
+   for(let i = 0; i<name.length; i++)
+   newArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+return newArray
+}
+
+
+
+
+function countDown(number) {
+    while (number >= 0){
+    console.log (number --)
+    } 
+}   
